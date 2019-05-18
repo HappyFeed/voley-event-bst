0854be61d28bb7faf2adc9f7f266dd53d76d1516
HappyFeed <alejomam2011@gmail.com>
2019-05-18 15:59:04 -0500

    first commit

---
+++ src/model/VolleyBallEvent.java
@@ -0,0 +1,120 @@
+package model;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javafx.scene.image.Image;
+
+public class VolleyBallEvent {
+	
+	private Participant root;
+	private Participant first;
+	
+	public static final String PATH = "data/data.csv";
+	
+	public VolleyBallEvent() {
+		
+	}
+	
+	public void addParticipantIntoTree(Participant part) {
+		addParticipantIntoTree(part, root);
+	}
+	
+	public void addParticipantIntoTree(Participant part, Participant current) {
+		if(root == null) {
+			root = part;
+		}
+		else {
+			if(part.compareTo(current) <= 0) {
+				if(current.getLeft() == null) {
+					current.setLeft(part);
+				}else{
+					addParticipantIntoTree(part, current.getLeft());
+				}
+			} else{
+				if(current.getRigth() == null) {
+					current.setRigth(part);
+				} else {
+					addParticipantIntoTree(part, current.getRigth());
+				}
+			}
+			
+		}
+	}
+	
+	public String LoadFileAndAddToTree() throws IOException {
+		File file = new File(PATH);
+		FileReader fileReader = new FileReader(file);
+		BufferedReader br = new BufferedReader(fileReader);
+		String line = br.readLine();
+		line = br.readLine();
+		while(line != null){
+			String[] temporalDataArray = line.split(",");
+			
+			URL url = new URL(temporalDataArray[6]);
+			URLConnection conn = url.openConnection();
+			InputStream in = conn.getInputStream();
+			Image img = new Image(in);
+			
+			Participant temporalNewParticipant = new Participant(Integer.parseInt(temporalDataArray[0]),temporalDataArray[1],temporalDataArray[2],temporalDataArray[3],temporalDataArray[4],temporalDataArray[5],img,temporalDataArray[7]);
+			addParticipantIntoTree(temporalNewParticipant);
+			line = br.readLine();
+		}
+		fileReader.close();
+		br.close();
+		
+		return PATH;
+	}
+	
+	public Participant searchParticipant(int id) {
+		Participant s= new Participant(id,"","","","","",null,"");
+		return searchParticipant(root,s);
+	}
+	private Participant searchParticipant(Participant current, Participant s) {
+		if(current!=null) {
+			if(s.compareTo(current)<0) {
+				if(current.getLeft()!=null){
+					return searchParticipant(current.getLeft(),s);
+				}else {
+					return searchParticipant(current.getRigth(), s);
+				}
+			}else if(s.compareTo(current)>0){
+				if(current.getRigth()!=null) {
+					return searchParticipant(current.getRigth(), s);
+				}else {
+					return searchParticipant(current.getLeft(), s);
+				}
+			}else {
+				return current;
+			}
+		}
+		return current;
+	}
+	
+	public Participant searchSpectator(int id) {
+		Participant result = root;
+		
+		return result;
+	}
+	
+	public Participant getRoot() {
+		return root;
+	}
+	
+	public void setRoot(Participant root) {
+		this.root = root;
+	}
+	
+	public Participant getFirst() {
+		return first;
+	}
+	
+	public void setFirst(Participant first) {
+		this.first = first;
+	}
+}
---
+++ src/ui/VolleyBallController.java
@@ -0,0 +1,143 @@
+package ui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import model.Participant;
+import model.VolleyBallEvent;
+
+public class VolleyBallController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField path;
+
+    @FXML
+    private Label message;
+
+    @FXML
+    private TextField idSpectators;
+
+    @FXML
+    private Label spectatorId;
+
+    @FXML
+    private Label timeSpectators;
+
+    @FXML
+    private TextField idParticipant;
+
+    @FXML
+    private Label participantId;
+
+    @FXML
+    private Label timeParticipant;
+
+    @FXML
+    private ImageView image;
+
+    @FXML
+    private Label data;
+    
+    @FXML
+    private Label id;
+
+    @FXML
+    private Label firstName;
+
+    @FXML
+    private Label lastName;
+
+    @FXML
+    private Label email;
+
+    @FXML
+    private Label gender;
+
+    @FXML
+    private Label country;
+
+    @FXML
+    private Label birthday;
+    
+    private VolleyBallEvent volleyBallEvent;
+
+    @FXML
+    void exportFile(ActionEvent event) {
+    	Stage stage = new Stage();
+    	FileChooser fileChooser = new FileChooser();
+    	fileChooser.setTitle("Open Resource File");
+    	fileChooser.showOpenDialog(stage);
+    }
+
+    @FXML
+    void loadFile(ActionEvent event) throws IOException {
+    	path.setText(volleyBallEvent.LoadFileAndAddToTree());
+    	message.setVisible(true);
+    }
+
+    @FXML
+    void searchParticipant(ActionEvent event) {
+    	long time = System.currentTimeMillis();
+    	try {
+    		int id = Integer.parseInt(idParticipant.getText());
+    		showDataInScreen(volleyBallEvent.searchParticipant(id));
+    	}catch (NumberFormatException e) {
+    		Alert score = new Alert(AlertType.ERROR);
+        	score.setTitle(" IV Copa Panamericana de Voleibol Masculino Sub-21");
+        	score.initStyle(StageStyle.DECORATED);
+        	score.setContentText("Please introduce a number");
+        	score.show();
+    	}
+    	timeParticipant.setText(""+ (System.currentTimeMillis()-time));
+    }
+
+    @FXML
+    void searchSpectators(ActionEvent event) {
+    	long time = System.currentTimeMillis();
+    	try {
+    		int id = Integer.parseInt(idSpectators.getText());
+    	}catch (NumberFormatException e) {
+    		Alert score = new Alert(AlertType.ERROR);
+        	score.setTitle(" IV Copa Panamericana de Voleibol Masculino Sub-21");
+        	score.initStyle(StageStyle.DECORATED);
+        	score.setContentText("Please introduce a number");
+        	score.show();
+    	}
+    	
+    	
+    	timeSpectators.setText(""+ (System.currentTimeMillis()-time));
+    }
+    
+    public void showDataInScreen(Participant p) {
+    	image.setImage(p.getPhoto());
+    	id.setText(p.getId()+"");
+    	firstName.setText(p.getFirstName());
+    	lastName.setText(p.getLastName());
+    	email.setText(p.getEmail());
+    	gender.setText(p.getGender());
+    	country.setText(p.getCountry());
+    	birthday.setText(p.getBirthday());
+    }
+
+    @FXML
+    void initialize() {
+    	volleyBallEvent = new VolleyBallEvent();
+    	message.setVisible(false);
+    }
+}
